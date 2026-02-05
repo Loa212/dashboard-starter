4232c29d51cd97d35ab68f27cd254396c01175b5
loa212 <anobileloris@gmail.com>
2026-02-05 16:57:16 +0100

    rm unused import

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -1,6 +1,5 @@
 import { Loader2 } from "lucide-react";
 import { greet } from "my-package-template";
-import {} from "my-pkg";
 import { useState } from "react";
 import { Button } from "~/components/ui/button";
 import { Card, CardContent } from "~/components/ui/card";
