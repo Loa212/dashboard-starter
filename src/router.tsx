a415385622797d22e5be7babb011e5c04a73df7d
loa212 <anobileloris@gmail.com>
2026-02-08 12:38:40 +0100

    feat: setupRouterSsrQueryIntegration

--- src/router.tsx
+++ src/router.tsx
@@ -1,13 +1,24 @@
 import { createRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
+import { QueryClient } from '@tanstack/react-query'
+import { setupRouterSsrQueryIntegration } from '@tanstack/react-router-ssr-query'
+
 
 export function getRouter() {
+    const queryClient = new QueryClient()
+  
   const router = createRouter({
     routeTree,
+    context: { queryClient},
     defaultPreload: 'intent',
     defaultErrorComponent: () => <p>Error</p>,
     defaultNotFoundComponent: () => <p>Not Found</p>,
     scrollRestoration: true,
+  })
+
+  setupRouterSsrQueryIntegration({
+    router,
+    queryClient,
   })
   return router
 }
